9f225b4dc36e10518422c1e94c042bf9c3329d46
SuchDominion <93291783+SuchDominion@users.noreply.github.com>
2024-05-01 03:49:15 -0500

    us server

--- Assets/Photon/PhotonRealtime/Code/LoadBalancingClient.cs
+++ Assets/Photon/PhotonRealtime/Code/LoadBalancingClient.cs
@@ -358,7 +358,8 @@ namespace Photon.Realtime
         public bool IsUsingNameServer { get; set; }
 
         /// <summary>Name Server Host Name for Photon Cloud. Without port and without any prefix.默认 ns.photonengine.io</summary>
-        public string NameServerHost = "ns.photonengine.cn";
+        // public string NameServerHost = "ns.photonengine.cn";
+        public string NameServerHost = "ns.photonengine.io";
 
         /// <summary>Name Server Address for Photon Cloud (based on current protocol). You can use the default values and usually won't have to set this value.</summary>
         public string NameServerAddress { get { return this.GetNameServerAddress(); } }
