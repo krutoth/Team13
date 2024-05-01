d980559cc5e3a2072f2d1d1a4610058b905b6470
SuchDominion <93291783+SuchDominion@users.noreply.github.com>
2024-05-01 10:10:45 -0500

    manually merged, works on PC

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -18,10 +18,7 @@ public class Raycast : MonoBehaviour
     private GameObject seeker;
     private bool isHidden = false;
     private Vector3 lastPosition;
-<<<<<<< HEAD
-=======
     private Quaternion lastRotation;
->>>>>>> Kuei-Yu
     public GameObject startMenu;
     public GameObject roleMenu;
     public GameObject endMenu;
@@ -31,9 +28,6 @@ public class Raycast : MonoBehaviour
     public TextMeshProUGUI statusText;
     private float timeRemaining;
     private bool countdownStarted = false;
-<<<<<<< HEAD
-    public GameObject Gate;
-=======
     public GameObject Gate; // Main gate
     public GameObject playerVisual;
     public GameObject hiderVisual;
@@ -41,7 +35,6 @@ public class Raycast : MonoBehaviour
     private bool end = true;
     private int maxNPCs = 3; // The maximum number of NPCs allowed
     private Transform[] hidePlaces; // Array to store hide place positions
->>>>>>> Kuei-Yu
     private GameObject[] currentHider;
 
     void Start()
@@ -52,14 +45,11 @@ public class Raycast : MonoBehaviour
         lineRenderer.startWidth = 0.001f;
         lineRenderer.endWidth = 0.001f;
 
-<<<<<<< HEAD
         Vector3 menuPosition = mainCamera.transform.position + mainCamera.transform.forward * 0.1f;
         startMenu.transform.position = menuPosition;
         startMenu.transform.rotation = Camera.main.transform.rotation;
         startMenu.SetActive(true);
-=======
         openMenu(startMenu);
->>>>>>> Kuei-Yu
         CharacterMovement targetScript = playerObject.GetComponent<CharacterMovement>();
         targetScript.enabled = false;
     }
@@ -69,13 +59,6 @@ public class Raycast : MonoBehaviour
     void Update()
     {
         currentHider = GameObject.FindGameObjectsWithTag("Hider");
-<<<<<<< HEAD
-=======
-        if (currentHider.Length == 0 && !end)
-        {
-            gameEnd(true);
-        }
->>>>>>> Kuei-Yu
 
         Vector3 rayOrigin = mainCamera.transform.position - mainCamera.transform.up * 0.025f;
         Ray ray = new Ray(rayOrigin, mainCamera.transform.forward);
@@ -118,14 +101,10 @@ public class Raycast : MonoBehaviour
                     if (Input.GetAxis("js0") != 0 || Input.GetAxis("js11") != 0 || Input.GetAxis("js20") != 0 || Input.GetKeyDown(KeyCode.Y))
                     {
                         startMenu.SetActive(false);
-<<<<<<< HEAD
                         Vector3 menuPosition = mainCamera.transform.position + mainCamera.transform.forward * 0.1f;
                         roleMenu.transform.position = menuPosition;
                         roleMenu.transform.rotation = Camera.main.transform.rotation;
                         roleMenu.SetActive(true);
-=======
-                        openMenu(roleMenu);
->>>>>>> Kuei-Yu
                     }
                 }
                 else if (objectName == "Multiple") // Multiple game
@@ -134,31 +113,17 @@ public class Raycast : MonoBehaviour
                     {
                         // Modified your multiplayer games starting setup here
                         startMenu.SetActive(false);
-<<<<<<< HEAD
                         Vector3 menuPosition = mainCamera.transform.position + mainCamera.transform.forward * 0.1f;
                         roleMenu.transform.position = menuPosition;
                         roleMenu.transform.rotation = Camera.main.transform.rotation;
                         roleMenu.SetActive(true);
 
-=======
-                        openMenu(roleMenu);
->>>>>>> Kuei-Yu
                     }
                 }
                 else if (objectName == "Hider" || objectName == "Seeker") // Set character as a hider / seeker
                 {
                     if (Input.GetAxis("js0") != 0 || Input.GetAxis("js11") != 0 || Input.GetAxis("js20") != 0 || Input.GetKeyDown(KeyCode.Y))
                     {
-<<<<<<< HEAD
-                        playerObject.tag = objectName;
-                       roleMenu.SetActive(false);
-                       // StartCoroutine(hideTime());
-
-                      _CodeAssistance.CallDUO();
-                   }
-
-                   //_CodeAssistance.CallDUO();
-=======
                         foreach (Transform child in playerVisual.transform)
                         {
                             // Destroy the child object
@@ -199,7 +164,6 @@ public class Raycast : MonoBehaviour
                         roleMenu.SetActive(false);
                         StartCoroutine(hideTime());
                     }
->>>>>>> Kuei-Yu
 
                 }
                 else if (objectName == "Restart")
@@ -207,10 +171,6 @@ public class Raycast : MonoBehaviour
                     if (Input.GetAxis("js0") != 0 || Input.GetAxis("js11") != 0 || Input.GetAxis("js20") != 0 || Input.GetKeyDown(KeyCode.Y))
                     {
                         playerObject.tag = "Untagged";
-<<<<<<< HEAD
-                        endMenu.SetActive(false);
-                        startMenu.SetActive(true);
-=======
                         // Find all GameObjects with tags "Hider", "Seeker", or "Touched"
                         GameObject[] hiderToDelete = GameObject.FindGameObjectsWithTag("Hider");
                         GameObject[] seekerObjects = GameObject.FindGameObjectsWithTag("Seeker");
@@ -237,7 +197,6 @@ public class Raycast : MonoBehaviour
                         MainDoors();
                         endMenu.SetActive(false);
                         openMenu(startMenu);
->>>>>>> Kuei-Yu
                     }
                 }
             }
@@ -250,13 +209,10 @@ public class Raycast : MonoBehaviour
                 outline.OutlineWidth = 10f;
             }
 
-<<<<<<< HEAD
             // Use Y Button
             // Kuei-Yu: js11
             // Ryan: js20
             // Android: js0
-=======
->>>>>>> Kuei-Yu
             if (Input.GetAxis("js0") != 0 || Input.GetAxis("js11") != 0 || Input.GetAxis("js20") != 0 || Input.GetKeyDown(KeyCode.Y))
             {
                 string gazedObjectName = hit.collider.gameObject.name;
@@ -338,14 +294,11 @@ public class Raycast : MonoBehaviour
         // Android: js2
         if ((Input.GetAxisRaw("js2") != 0 || Input.GetAxisRaw("js7") != 0 || Input.GetAxisRaw("js15") != 0 || Input.GetKeyDown(KeyCode.B)) && !menu.activeSelf)
         {
-<<<<<<< HEAD
             Vector3 menuPosition = mainCamera.transform.position + mainCamera.transform.forward * 0.1f;
             menu.transform.position = menuPosition;
             menu.transform.rotation = Camera.main.transform.rotation;
             menu.SetActive(true);
-=======
             openMenu(menu);
->>>>>>> Kuei-Yu
             CharacterMovement targetScript = playerObject.GetComponent<CharacterMovement>();
             targetScript.enabled = false;
         }
@@ -358,29 +311,12 @@ public class Raycast : MonoBehaviour
             {
                 timeRemaining = 0f;
                 countdownStarted = false;
-<<<<<<< HEAD
                 // Handle countdown completion here
                 Debug.Log("Countdown completed!");
-=======
->>>>>>> Kuei-Yu
             }
 
             UpdateCountdownDisplay();
         }
-<<<<<<< HEAD
-    }
-
-    void MainDoors()
-    {
-        Vector3 DoorRotate = new Vector3(0, 0, 90);
-        if (leftDoor.localRotation == Quaternion.Euler(-90, 0, 0))
-        {
-            leftDoor.localRotation *= Quaternion.Euler(-DoorRotate);
-            rightDoor.localRotation *= Quaternion.Euler(DoorRotate);
-        }
-        else
-        {
-=======
         // Got touched
         if (playerObject.tag == "Touched" && !end)
         {
@@ -426,7 +362,6 @@ public class Raycast : MonoBehaviour
         }
         else
         {
->>>>>>> Kuei-Yu
             leftDoor.localRotation *= Quaternion.Euler(DoorRotate);
             rightDoor.localRotation *= Quaternion.Euler(-DoorRotate);
         }
@@ -474,11 +409,7 @@ public class Raycast : MonoBehaviour
             lastRotation = playerObject.transform.rotation;
             CharacterMovement targetScript = playerObject.GetComponent<CharacterMovement>();
             targetScript.enabled = false;
-<<<<<<< HEAD
-            Vector3 offset = new Vector3(0f, -0.5f, 0f);
-=======
             Vector3 offset = new Vector3(0.04f, 0.2f, 0.3f);
->>>>>>> Kuei-Yu
             playerObject.transform.position = bedPosition + offset;
             playerObject.transform.rotation = bedRotation;
             playerObject.transform.rotation *= Quaternion.Euler(0, 0, 90);
@@ -498,11 +429,8 @@ public class Raycast : MonoBehaviour
     {
         // Disable the hider
         hider.SetActive(false);
-<<<<<<< HEAD
-=======
         hider.tag = "Touched";
         hider.transform.parent.parent.tag = "Touched";
->>>>>>> Kuei-Yu
     }
 
     IEnumerator hideTime()
@@ -523,22 +451,16 @@ public class Raycast : MonoBehaviour
         yield return new WaitForSeconds(30);
         countdownText.text = "";
         StartCoroutine(startTime());
-<<<<<<< HEAD
-=======
         if(playerObject.tag == "Hider")
         {
             GameObject NPC = Instantiate(seekerVisual.transform.parent.parent.gameObject, new Vector3(-50, -2, 2), Quaternion.identity);
             NPC.SetActive(true);
         }
->>>>>>> Kuei-Yu
     }
 
     IEnumerator startTime()
     {
-<<<<<<< HEAD
-=======
         end = false;
->>>>>>> Kuei-Yu
         leftDoor = Gate.transform.Find("Left Door").transform;
         rightDoor = Gate.transform.Find("Right Door").transform;
         MainDoors();
@@ -561,7 +483,6 @@ public class Raycast : MonoBehaviour
         }
         else
         {
-<<<<<<< HEAD
             if (currentHider.Length == 0)
             {
                 gameEnd(true);
@@ -570,9 +491,7 @@ public class Raycast : MonoBehaviour
             {
                 gameEnd(false);
             }
-=======
             gameEnd(false);
->>>>>>> Kuei-Yu
         }
     }
     void UpdateCountdownDisplay()
@@ -584,25 +503,24 @@ public class Raycast : MonoBehaviour
         countdownText.text = "Time Remaining: " + seconds.ToString() + "s";
     }
 
-<<<<<<< HEAD
-    void gameEnd(bool win)
-    {
-        if (win)
-        {
-            statusText.text = "You Win!";
-        }
-        else
-        {
-            statusText.text = "You Lose!";
-        }
-        leftDoor = Gate.transform.Find("Left Door").transform;
-        rightDoor = Gate.transform.Find("Right Door").transform;
-        MainDoors();
-        Vector3 menuPosition = mainCamera.transform.position + mainCamera.transform.forward * 0.1f;
-        endMenu.transform.position = menuPosition;
-        endMenu.transform.rotation = Camera.main.transform.rotation;
-        endMenu.SetActive(true);
-=======
+    // void gameEnd(bool win)
+    // {
+    //     if (win)
+    //     {
+    //         statusText.text = "You Win!";
+    //     }
+    //     else
+    //     {
+    //         statusText.text = "You Lose!";
+    //     }
+    //     leftDoor = Gate.transform.Find("Left Door").transform;
+    //     rightDoor = Gate.transform.Find("Right Door").transform;
+    //     MainDoors();
+    //     Vector3 menuPosition = mainCamera.transform.position + mainCamera.transform.forward * 0.1f;
+    //     endMenu.transform.position = menuPosition;
+    //     endMenu.transform.rotation = Camera.main.transform.rotation;
+    //     endMenu.SetActive(true);
+    // }
     void SpawnNPC()
     {
         // Instantiate the NPC prefab
@@ -621,7 +539,6 @@ public class Raycast : MonoBehaviour
             NPC[i].transform.rotation *= Quaternion.Euler(0, 0, 90);
             NPC[i].SetActive(true);
         }
->>>>>>> Kuei-Yu
     }
 
     void DrawRay(Vector3 start, Vector3 end)
@@ -631,7 +548,7 @@ public class Raycast : MonoBehaviour
         lineRenderer.SetPosition(1, end);
     }
 
-    private void RestoreOriginalColor()
+    void RestoreOriginalColor()
     {
         if (lastHitObject != null)
             Destroy(lastHitObject.GetComponent<Outline>());
