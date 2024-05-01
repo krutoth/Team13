a17ed4604b4e364a42457de291fa87da51da95b7
krutoth <94133905+krutoth@users.noreply.github.com>
2024-05-01 10:51:00 -0500

    Update SpeedPickup.cs

--- Assets/ENV_ASSET/Speed Interactable/SpeedPickup.cs
+++ Assets/ENV_ASSET/Speed Interactable/SpeedPickup.cs
@@ -37,7 +37,7 @@ public class SpeedPickup : MonoBehaviour
 
             // Change speed on CharacterMovement.cs of Character object (Parent objects of Capsule and Clone)
             GameObject.Find("Character").GetComponent<CharacterMovement>().speed += speedBoost;
-            // other.gameObject.GetComponent<CharacterMovement>().speed += speedBoost;
+            // set speedBoost to true in RayC.cs
 
             if (respawn)
             {
